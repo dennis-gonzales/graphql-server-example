b56add4ad0c44c1dcd6de45367586ad4a8ffe6f3
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2023-10-06 15:29:32 +0800

    refactor: improve typing of resolver

--- src/resolvers/resolve-search.ts
+++ src/resolvers/resolve-search.ts
@@ -1,14 +1,18 @@
-import type { SearchResultResolvers } from '../__generated__/resolvers-types';
+import type {
+  Author,
+  Book,
+  SearchResultResolvers,
+} from '../__generated__/resolvers-types';
 import { hasPropertyWithKey } from '../utils/guard';
 
 const searchResolver: SearchResultResolvers = {
   __resolveType(obj, contextValue, info) {
     // Only Author has a name field
-    if (hasPropertyWithKey(obj, 'name')) {
+    if (hasPropertyWithKey<keyof Author>(obj, 'name')) {
       return 'Author';
     }
     // Only Book has a title field
-    if (hasPropertyWithKey(obj, 'title')) {
+    if (hasPropertyWithKey<keyof Book>(obj, 'title')) {
       return 'Book';
     }
   },
